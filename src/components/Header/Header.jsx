2b541ba429e3752946a3322f3e2b167b41703d52
moetez96 <moetez.ayari@esprit.tn>
2021-09-13 17:10:39 +0100

    Map component + Google maps

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
     return (
         <AppBar position="static">
             <Toolbar className={classes.toolbar}>
-                <Typography variant="h5" classeName={classes.title}>
+                <Typography variant="h5" className={classes.title}>
                     Travel Advisor
                 </Typography>
                 <Box display="flex">
-                <Typography variant="h6" classeName={classes.title}>
+                <Typography variant="h6" className={classes.title}>
                     Explore other places
                 </Typography>
                 {/*<Autocomplete> */}
